3fa1a611b79842b549ab2eaf39366dde214cdd41
JonathanMorocho <jamorocho@tecnologicoloja.edu.ec>
2021-09-12 11:04:23 -0500

    Gestion

---
+++ Academico/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Carrera(models.Model):
+    codigo = models.CharField(max_length=3, primary_key=True)
+    Periodo_Academico = models.CharField(max_length=25)
+    Fecha_inicio_Periodo = models.DateField()
+    Duracion_Ciclo = models.CharField(max_length=25)
+
+class Estudiante(models.Model):
+    dni = models.CharField(max_length=10, primary_key=True)
+    Nombres = models.CharField(max_length=25)
+    Apellidos = models.CharField(max_length=25)
+    Fecha_Nacimiento = models.DateField()
+    Edad = models.IntegerField()
+    sexos = [
+        ('H', 'Masculino'),
+        ('F', 'Femenino')
+    ]
+    sexo = models.CharField(max_length=1, choices=sexos, default='H')
+    PeriodoAcademico = models.ForeignKey(Carrera, null=False, blank=False, on_delete=models.CASCADE)
+
+    def nombreCompleto(self):
+        txt = "{0} {1}, {2}"
+        return txt.format(self.Nombres, self.Apellidos)
