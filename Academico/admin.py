69443bdda2549c100137ac279dcb4cbfd830fce2
JonathanMorocho <jamorocho@tecnologicoloja.edu.ec>
2021-09-13 21:33:14 -0500

    Gestion Academica

--- Academico/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Academico.models import *
-# Register your models here.
-admin.site.register(Carrera)
-admin.site.register(Estudiante)
--- Academico/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import *
-
-class RegistroForm(forms.ModelForm):
-    class Meta:
-        
-        model = Carrera 
-        fields = '__all__'
